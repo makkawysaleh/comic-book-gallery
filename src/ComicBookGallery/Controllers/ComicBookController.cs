723b464ac4f786d9a1a9f66e1e396dd3de4418da
Saleh <Saleh@DESKTOP-RFR0LD8>
2016-10-11 21:20:21 -0700

    Added design files

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -8,7 +8,21 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBookController: Controller
     {
+        
         public ActionResult  Detail() {
+            // ViewBag is a Razar type that can hold nay data type
+            // to allow us to use them in the view folder.
+            ViewBag.SeriesTitle = "The Amazing Spider-Man"; 
+            ViewBag.IssueNumber = 700;
+            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+            ViewBag.Artists = new string[]
+            {
+            "script: dan slott",
+            "pencils: humberto ramos",
+            "inks: victor olazaba",
+            "colors: edgar delgado",
+            "letters: chris eliopoulos"
+            };
             return View();
         } 
     }
